78a2e2e0230faf5150b82d81ed98b8e39d8ba022
vollborn <oliver.vollborn@gmail.com>
2021-09-11 13:39:58 +0200

    BuildTools

--- src/run.sh
+++ src/run.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-if [ "${4}" != "true" ]; then
+if [ "${EULA}" != "true" ]; then
 	echo "Please accept the eula first."
 	exit
 fi
 
 serverPath="/opt/minecraft-server"
+buildToolsPath="/opt/build-tools"
+buildToolsJarPath="${buildToolsPath}/BuildTools.jar"
 
 if [ ! -d "${serverPath}" ]; then
 	echo "Please pass the ${serverPath} directory for persistent storage!"
 	exit
 fi
-
-version=${1}
-ramMin=${2}
-ramMax=${3}
 
 echo ""
 echo "  ░██████╗██████╗░██╗░██████╗░░█████╗░████████╗"
@@ -24,28 +22,74 @@ echo "  ░╚═══██╗██╔═══╝░██║██║░░
 echo "  ██████╔╝██║░░░░░██║╚██████╔╝╚█████╔╝░░░██║░░░"
 echo "  ╚═════╝░╚═╝░░░░░╚═╝░╚═════╝░░╚════╝░░░░╚═╝░░░"
 echo ""
-echo "                    DOCKERIZED "
+echo "                   DOCKERIZED "
 echo ""
 echo ""
-echo "Version      →   ${version}"
-echo "Memory Min   →   ${ramMin}"
-echo "Memory Max   →   ${ramMax}"
+echo "Version      →   ${VERSION}"
+echo "Memory Min   →   ${MEMORY_MIN}"
+echo "Memory Max   →   ${MEMORY_MAX}"
 echo ""
 
 
-cd ${serverPath}
+if [ "${BUILD_ON_START}" == "true" ]; then
 
+	# Download BuildTools
 
-# Download server.jar
+	if [ ! -d "${buildToolsPath}" ]; then
+		mkdir "${buildToolsPath}"
+	fi
 
-if [ ! -f "./server.jar" ]; then
-	echo "Downloading server.jar..."
-	curl -o server.jar  https://cdn.getbukkit.org/spigot/spigot-${version}.jar &> /dev/null
+	if [ ! -f "${buildToolsJarPath}" ]; then
+		echo "Downloading BuildTools..."
+		curl -o "${buildToolsJarPath}" https://hub.spigotmc.org/jenkins/job/BuildTools/lastStableBuild/artifact/target/BuildTools.jar &> /dev/null
+	fi
 
-	if [ ! -f "./server.jar" ]; then
-		echo "Server.jar could not be downloaded. Aborting..."
+	if [ ! -f "${buildToolsJarPath}" ]; then
+		echo "Downloading BuildTools failed."
+		echo "Exiting..."
 		exit
 	fi
+
+
+	# Build spigot.jar
+
+	cd "${buildToolsPath}"
+
+	echo "Building..."
+	echo "This may take a while."
+	echo ""
+	if [ -f "${serverPath}/server.jar" ]; then
+		java -jar BuildTools.jar --rev ${VERSION} --compile-if-changed
+	else
+		java -jar BuildTools.jar --rev ${VERSION}
+	fi
+	echo ""
+
+
+	# Move builded file
+
+	if [ -f "./spigot-${VERSION}.jar" ]; then
+		mv "./spigot-${VERSION}.jar" "${serverPath}/server.jar"
+	fi
+
+	cd "${serverPath}"
+
+else
+
+	cd "${serverPath}"
+
+	# Download server.jar
+
+	if [ ! -f "./server.jar" ]; then
+		echo "Downloading server.jar..."
+		curl -o server.jar  https://cdn.getbukkit.org/spigot/spigot-${VERSION}.jar &> /dev/null
+
+		if [ ! -f "./server.jar" ]; then
+			echo "Server.jar could not be downloaded. Aborting..."
+			exit
+		fi
+	fi
+
 fi
 
 
@@ -61,7 +105,7 @@ fi
 
 echo "Starting server..."
 echo ""
-java -jar -Xms${ramMin} -Xmx${ramMax} server.jar
+java -jar -Xms${MEMORY_MIN} -Xmx${MEMORY_MAX} server.jar
 
 
 # Server stop
